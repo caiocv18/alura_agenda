f1a7e152707fa4ea77b8dceea37ae4b318cf1afa
caiocv18 <caioviniciuscv18@gmail.com>
2021-10-12 16:39:34 -0300

    Class 2 - Adding ListView

--- app/src/main/java/br/com/caiocv18/MainActivity.kt
+++ app/src/main/java/br/com/caiocv18/MainActivity.kt
@@ -2,14 +2,23 @@ package br.com.caiocv18
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"Jesus Alive!!! Caio Vinicius", Toast.LENGTH_LONG).show()
+
+        val alunos : List<String> = ArrayList(Arrays.asList("Alex", "Fran", "Jose","Caio", "Iara"))
+
+        val listaDeAlunos : ListView = findViewById(R.id.activity_main_lista_de_alunos)
+        listaDeAlunos.setAdapter(ArrayAdapter(this, android.R.layout.simple_list_item_1,alunos))
 
     }
 }
